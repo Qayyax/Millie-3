32514fbd131f1a1dd0a88e68e197ad4bccd02817
Qayyax <qayyax@gmail.com>
2023-01-25 19:04:10 +0100

    added functions

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict';
-const welcome = $('');
+
+document.querySelector('.propose').addEventListener('click', function () {
+  const result = confirm('Will you be my Girl friend?');
+  if (result == true) {
+    alert('I love you to the moon');
+  } else {
+    alert('Are you sure');
+  }
+});
